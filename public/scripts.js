a174ae4c8a45933738b1e8d8554b03e3521f4fe7
wiii-1 <markejunemarino@gmail.com>
2025-05-17 16:17:04 +0800

    idk

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,4 @@
-const backendUrl = 'https://9447-2404-3c00-4e2f-b710-dc48-e99e-3530-1910.ngrok-free.app';
+const backendUrl = 'https://e9d7-2404-3c00-4e2f-b710-dc48-e99e-3530-1910.ngrok-free.app';
 
 document.getElementById('suggestion-form').addEventListener('submit', async (e) => {
     e.preventDefault();
