58149df794361044ee8f432e1eaedf7bbfd6bcef
wiii-1 <markejunemarino@gmail.com>
2025-05-17 16:21:28 +0800

    idk

--- utils/db.js
+++ utils/db.js
@@ -2,9 +2,11 @@ const { Pool } = require('pg');
 
 
 const pool = new Pool({
-  connectionString: 'postgres://Suggestions_owner:npg_E9pAC0eOwSKP@ep-rapid-smoke-a1vwvg99-pooler.ap-southeast-1.aws.neon.tech:5432/Suggestions?sslmode=require',
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
-
 
 (async () => {
   try {
