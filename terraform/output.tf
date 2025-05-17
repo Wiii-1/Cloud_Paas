a95768e6f19d23479acd997e2466c67fb386858f
wiii-1 <markejunemarino@gmail.com>
2025-05-17 16:01:29 +0800

    Remove ignored files from repo

--- terraform/output.tf
+++ terraform/output.tf
@@ -3,3 +3,6 @@ output "vercel_project_url" {
   description = "The URL of the deployed Vercel project"
 }
 
+output "database_url" {
+  value = "jdbc:postgresql://ep-rapid-smoke-a1vwvg99-pooler.ap-southeast-1.aws.neon.tech/Suggestions?user=Suggestions_owner&password=npg_E9pAC0eOwSKP&sslmode=require"
+}
--- terraform/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-vercel_token  = "9G0tyJ6yFr6FTKCZMJq6svpA"
-database_url  = "jdbc:postgresql://ep-rapid-smoke-a1vwvg99-pooler.ap-southeast-1.aws.neon.tech/Suggestions?user=Suggestions_owner&password=npg_E9pAC0eOwSKP&sslmode=require"
-
